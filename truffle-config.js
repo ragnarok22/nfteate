36db1017489d7050cd5daacd638ad527b5f7e6cf
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-03 16:45:05 -0400

    fix: typo

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = fs.readFileSync(".secrets").toString().trim();
 
 module.exports = {
   /**
