c4b9b3994b72296c8f7732b29b2a63413c9ae80f
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-04 13:58:25 -0400

    chore: agregando dependencias faltantes

---
+++ contracts/Lugonials.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./ERC721Tradable.sol";
+
+/**
+ * @title Lugonial
+ * Lugonial - a contract for my non-fungible lugonials.
+ */
+contract Creature is ERC721Tradable {
+    constructor(address _proxyRegistryAddress)
+        ERC721Tradable("Lugonial", "LUGO", _proxyRegistryAddress)
+    {}
+
+    function baseTokenURI() override public pure returns (string memory) {
+        return "https://nfteate.vercel.app/api/lugonials/";
+    }
+
+    function contractURI() public pure returns (string memory) {
+        return "https://creatures-api.opensea.io/contract/opensea-creatures";
+    }
+}
