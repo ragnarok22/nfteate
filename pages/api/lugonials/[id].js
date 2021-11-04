bcc9049595522a6804bcdfdda10546822de78dce
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-04 17:47:07 -0400

    fix: agregando mas lugonials

--- pages/api/lugonials/[id].js
+++ pages/api/lugonials/[id].js
@@ -1,17 +1,8 @@
+import lugonials from '../lugonials.json'
+
 export default function helloAPI(req, res) {
   const { id } = req.query;
-  const lugonial = {
-    name: 'Reinier Hernández',
-    description: 'Reinier is a software developer and a web developer. He is a passionate and creative person. He is a lover of technology and technology is his passion.',
-    image: 'https://avatars3.githubusercontent.com/u/12098981?s=460&v=4',
-    external_url: null,
-    attributes: [
-      {
-        trait_type: 'Número',
-        value: id
-      }
-    ]
-  }
+  const lugonial = lugonials.find(lugonial => lugonial.id === id);
 
-  res.status(200).json({ ...lugonial, id });
+  res.status(200).json({ ...lugonial });
 }
