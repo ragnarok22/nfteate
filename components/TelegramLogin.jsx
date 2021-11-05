9abb60c5b72b5f372aea3c3b588b02a3108c4f7a
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-05 13:26:50 -0400

    fix: user params

--- components/TelegramLogin.jsx
+++ components/TelegramLogin.jsx
@@ -6,7 +6,8 @@ const TelegramLogin = () => {
 
   const onTelegramAuth = (user) => {
     console.log(user);
-    router.push(`/mint?name=${user.name}&image=${user.image}`);
+    // auth_date, first_name, hash, id, last_name, photo:url, username
+    router.push(`/mint?name=${user.first_name} ${user.last_name}&image=${user.photo_url}&username=${user.username}`);
   };
   return (
     <TelegramLoginButton dataOnauth={onTelegramAuth} botName="nolodigasbot" />
