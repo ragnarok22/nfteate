ed107ee8cba176da7509e677e3902a8ffdaf83eb
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-05 12:36:08 -0400

    feat: fetch all lugonials

--- pages/api/lugonials/index.js
+++ pages/api/lugonials/index.js
@@ -1,3 +1,17 @@
-export default function helloAPI(req, res) {
-  res.status(200).json({ name: 'Lugonials' })
+import { supabase } from "../../../utils/supabaseClient"
+
+export default async function helloAPI(req, res) {
+  try {
+    let { data, error, status } = await supabase.from("lugonials").select("*")
+    
+    if (error && status !== 406) {
+      throw error
+    }
+    
+    if (data) {
+      res.status(200).json(data)
+    }
+  } catch (error) {
+    res.status(500).json({ error })
+  }
 }
