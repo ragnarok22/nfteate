fbba337372767c63a005b73ca90aa29fad84f689
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-05 13:34:32 -0400

    fix: image dimension on profile

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Profile = ({ image, name }) => {
   return (
     <div>
-      <Image src={image} />
+      <Image src={image} width="300" height="300" alt="profile" />
       <h1>{name}</h1>
     </div>
   )
--- pages/mint.jsx
+++ pages/mint.jsx
@@ -4,10 +4,15 @@ import Profile from "../components/Profile"
 const MintProfile = () => {
   const router = useRouter()
 
+  const handleMint = (e) => {
+    e.preventDefault()
+  }
+
   const { name, image } = router.query
   return (
     <div>
       <Profile image={image} name={name} />
+      <button onClick={handleMint}>Mintear</button>
     </div>
   )
 }
